81c1316a6a98cc47d3539387784834d0fdfa3d4f
JeramTough <1171867004@qq.com>
2017-11-26 21:31:58 +0800

    完成CreateTravelnoteActivity的UI业务

--- app/src/main/java/com/jeramtough/niyouji/controller/activity/CreateTravelnoteActivity.java
+++ app/src/main/java/com/jeramtough/niyouji/controller/activity/CreateTravelnoteActivity.java
@@ -8,10 +8,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.VideoView;
+import android.widget.*;
 import com.jeramtough.jtandroid.jtlog2.P;
 import com.jeramtough.niyouji.R;
 import com.jeramtough.niyouji.controller.dialog.SelectTakephotoOrVideoDialog;
@@ -21,12 +18,17 @@ import com.jeramtough.niyouji.controller.dialog.SelectTakephotoOrVideoDialog;
  */
 public class CreateTravelnoteActivity extends BaseActivity
 {
+	public final static String COVER_PATH_NAME="coverPath";
+	public final static String TITLE_NAME="title";
+	
 	private ImageView imageViewAddTravelnoteCover;
 	private ImageView imageViewTravelnoteCover;
 	private EditText editTravelnoteTitle;
 	private Button btnStartPerform;
 	private VideoView videoViewTravelnoteCover;
 	
+	private String coverPath;
+	private String title;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -60,6 +62,22 @@ public class CreateTravelnoteActivity extends BaseActivity
 				dialog.show();
 				break;
 			case R.id.btn_start_perform:
+				title = editTravelnoteTitle.getText().toString();
+				if (title.length() ==0)
+				{
+					Toast.makeText(this, "请输入游记标题！", Toast.LENGTH_SHORT).show();
+					break;
+				}
+				if (coverPath.length()==0)
+				{
+					Toast.makeText(this, "没有拍摄游记封面哦！", Toast.LENGTH_SHORT).show();
+					break;
+				}
+				
+				Intent intent = new Intent(this, PerformingActivity.class);
+				intent.putExtra(COVER_PATH_NAME,coverPath);
+				intent.putExtra(TITLE_NAME,title);
+				startActivity(intent);
 				break;
 		}
 	}
@@ -72,6 +90,9 @@ public class CreateTravelnoteActivity extends BaseActivity
 				resultCode == TakePhotoActivity.TAKE_PHOTO_RESULT_CODE)
 		{
 			String photoPath = data.getStringExtra(TakePhotoActivity.PHOTO_PATH_NAME);
+			
+			this.coverPath=photoPath;
+			
 			Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
 			imageViewTravelnoteCover.setImageBitmap(bitmap);
 			
@@ -84,6 +105,7 @@ public class CreateTravelnoteActivity extends BaseActivity
 		else if (requestCode == resultCode && resultCode == VideoActivity.VIDEO_RESULT_CODE)
 		{
 			String videoPath = data.getStringExtra(VideoActivity.VIDEO_PATH_NAME);
+			this.coverPath=videoPath;
 			
 			editTravelnoteTitle.setBackgroundResource(R.mipmap.title_background);
 			editTravelnoteTitle.setTextColor(Color.WHITE);
