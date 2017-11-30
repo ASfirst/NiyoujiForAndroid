fc7d738fdf71d61e50a806078ccf47ce7e1e075c
JeramTough <1171867004@qq.com>
2017-11-30 13:54:23 +0800

    UI更新

--- app/src/main/java/com/jeramtough/niyouji/controller/activity/PerformingActivity.java
+++ app/src/main/java/com/jeramtough/niyouji/controller/activity/PerformingActivity.java
@@ -3,24 +3,27 @@ package com.jeramtough.niyouji.controller.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import com.jeramtough.jtandroid.ui.JtViewPager;
 import com.jeramtough.niyouji.R;
+import com.jeramtough.niyouji.component.ui.travelnote.LiveTravelnotePageHandler;
 
 /**
  * @author 11718
  */
 public class PerformingActivity extends AppCompatActivity
 {
-	
+	private LiveTravelnotePageHandler liveTravelnotePageHandler;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_performing);
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		/*this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
 		
-		
-		
+		JtViewPager viewPagerTravelnotePages = findViewById(R.id.viewPager_travelnote_pages);
+		liveTravelnotePageHandler=new LiveTravelnotePageHandler(this,
+				viewPagerTravelnotePages);
 	}
 }
