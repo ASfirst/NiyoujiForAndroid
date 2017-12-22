026f0132187ea0c2000436fa7592b167dc83800c
JeramTough <1171867004@qq.com>
2017-12-22 12:32:41 +0800

    Signed-off-by: JeramTough <1171867004@qq.com>

---
+++ jtemoji/src/main/java/com/jeramtough/jtemoji/JtEmojiUtils.java
@@ -0,0 +1,45 @@
+package com.jeramtough.jtemoji;
+
+import android.content.Context;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author JeramTough
+ *         on 2017  December 21 Thursday 12:26.
+ */
+
+public class JtEmojiUtils
+{
+	public static SpannableString getEmotionContent(final Context context,
+			final TextView textView, JtEmojisHandler jtEmojisHandler, String emojiContentStr)
+	{
+		SpannableString spannableString = new SpannableString(emojiContentStr);
+		
+		String regexEmotion = "\\[[a-z][0-9]+\\]";
+		Pattern patternEmotion = Pattern.compile(regexEmotion);
+		Matcher matcherEmotion = patternEmotion.matcher(emojiContentStr);
+		
+		while (matcherEmotion.find())
+		{
+			// 获取匹配到的具体字符
+			String placeholder = matcherEmotion.group();
+			// 匹配字符串的开始位置
+			int start = matcherEmotion.start();
+			// 利用表情名字获取到对应的图片
+			ImageSpan span = new ImageSpan(context,
+					JtEmojiCachesManager.getJtEmojiCachesManager()
+							.processNewJtEmojiBitmap(context.getResources(),
+									jtEmojisHandler.getJtEmojiByPlaceholder(placeholder),
+									(int) textView.getTextSize()));
+			spannableString.setSpan(span, start, start + placeholder.length(),
+					Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		}
+		return spannableString;
+	}
+}
